1b7bd23b46a268b221765b967fe3caa31af97f4a
Jishnu <jscode99@gmail.com>
2021-01-26 21:56:49 +0530

    Material-ui integration

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,12 +1,24 @@
-import React from 'react'
-import './Sidebar.css'
+import React from "react";
+import "./Sidebar.css";
+import SidebarOption from "./SidebarOption";
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
 
 function Sidebar() {
-    return (
-        <div className="sidebar">
-            <h1>Hi im a Sidebar</h1>
-        </div>
-    )
+  return (
+    <div className="sidebar">
+      {/*Spotify Logo */}
+      <img
+        className="sidebar_logo"
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt="Spotify Logo"
+      />
+      <SidebarOption Icon={HomeIcon} title="Home" />
+      <SidebarOption Icon={SearchIcon} title="Search" />
+      <SidebarOption Icon={LibraryMusicIcon} title="Your Library" />
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
